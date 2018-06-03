13821074ae22e3f3a44aed730ab5be5db89d9dcd
Natalie Arellano <natalie.p.arellano@gmail.com>
2018-06-03 14:45:06 -0400

    Refactor print formatting
    - Indent nested function calls

--- codewriter/codewriter.go
+++ codewriter/codewriter.go
@@ -31,10 +31,7 @@ func Write(c parser.Command) string {
 }
 
 func writeArithmetic(c parser.Command) string {
-	if c.Type != "C_ARITHMETIC" {
-		panic(fmt.Sprintf("Error: called writeArithmetic with invalid command: type %s", c.Type))
-	}
-	var ret string
+	var ret []string
 	switch c.Arg1 {
 	case "add":
 		ret = arithmeticAdd()
@@ -55,128 +52,137 @@ func writeArithmetic(c parser.Command) string {
 	case "not":
 		ret = arithmeticNot()
 	}
-	return ret + incrementSP()
+	ret = append(ret, incrementSP()...)
+	return strings.Join(ret, "\n")
 }
 
 func writeGoto(c parser.Command) string {
-	return fmt.Sprintf("// Adding jump to %s\n", c.Arg1) +
-		fmt.Sprintf("@%s\n", c.Arg1) +
-		"0;JMP\n" +
-		"// done\n"
+	return strings.Join(indentedLines(comment(fmt.Sprintf("Adding jump to %s", c.Arg1)),
+		fmt.Sprintf("@%s", c.Arg1),
+		"0;JMP"), "\n")
 }
 
 func writeIf(c parser.Command) string {
-	return fmt.Sprintf("// Adding conditional jump to %s\n", c.Arg1) +
-		decrementSP() +
-		dereferenceSP() +
-		"D=M\n" +
-		fmt.Sprintf("@%s\n", c.Arg1) +
-		"D;JNE\n" +
-		"// done\n"
+	var lines []string
+	lines = append(lines, comment(fmt.Sprintf("Adding conditional jump to %s", c.Arg1)))
+	lines = append(lines, decrementSP()...)
+	lines = append(lines, dereferenceSP()...)
+	lines = append(lines, "D=M")
+	lines = append(lines, fmt.Sprintf("@%s", c.Arg1))
+	lines = append(lines, "D;JNE")
+	return strings.Join(indentedLines(lines...), "\n")
 }
 
 func writeLabel(c parser.Command) string {
-	return "// Adding label\n" +
-		fmt.Sprintf("(%s)\n", c.Arg1) +
-		"// done\n"
+	return strings.Join(indentedLines(comment("Adding label"),
+		fmt.Sprintf("(%s)", c.Arg1)), "\n")
 }
 
 // Pop into segment from stack
 func writePop(segment string, index int) string {
-	return fmt.Sprintf("// pop %s %d\n", segment, index) +
-		dereferenceSegment(segment, index) +
-		"D=A\n" + // get the address to come back to
-		"@R13\n" +
-		"M=D\n" + // store the value in memory
-		decrementSP() +
-		dereferenceSP() +
-		"D=M\n" + // get the value to pop
-		"@R13\n" +
-		"A=M\n" +
-		"M=D\n" +
-		"// done\n"
+	var lines []string
+	lines = append(lines, comment(fmt.Sprintf("pop %s %d", segment, index)))
+	lines = append(lines, dereferenceSegment(segment, index)...)
+	lines = append(lines, "D=A") // get the address to come back to
+	lines = append(lines, "@R13")
+	lines = append(lines, "M=D") // store the value in memory
+	lines = append(lines, decrementSP()...)
+	lines = append(lines, dereferenceSP()...)
+	lines = append(lines, "D=M") // get the value to pop
+	lines = append(lines, "@R13")
+	lines = append(lines, "A=M")
+	lines = append(lines, "M=D")
+	return strings.Join(indentedLines(lines...), "\n")
 }
 
 // Push onto stack from segment
 func writePush(segment string, index int) string {
-	return fmt.Sprintf("// push %s %d\n", segment, index) +
-		loadSegment(segment, index) +
-		dereferenceSP() +
-		"M=D\n" +
-		incrementSP() +
-		"// done\n"
+	var lines []string
+	lines = append(lines, comment(fmt.Sprintf("push %s %d", segment, index)))
+	lines = append(lines, loadSegment(segment, index)...)
+	lines = append(lines, dereferenceSP()...)
+	lines = append(lines, "M=D")
+	lines = append(lines, incrementSP()...)
+	return strings.Join(indentedLines(lines...), "\n")
 }
 
 // Helper Methods
-func arithmeticAdd() string {
-	return "// Adding x and y\n" +
-		loadXY() +
-		"M=D+M\n" +
-		"// done\n"
+func arithmeticAdd() []string {
+	var lines []string
+	lines = append(lines, comment("Adding x and y"))
+	lines = append(lines, loadXY()...)
+	lines = append(lines, "M=D+M")
+	return indentedLines(lines...)
 }
 
-func arithmeticSub() string {
-	return "// Subtracting y from x\n" +
-		loadXY() +
-		"M=M-D\n" +
-		"// done\n"
+func arithmeticSub() []string {
+	var lines []string
+	lines = append(lines, comment("Subtracting y from x"))
+	lines = append(lines, loadXY()...)
+	lines = append(lines, "M=M-D")
+	return indentedLines(lines...)
 }
 
-func arithmeticNeg() string {
-	return "// Negating x\n" +
-		decrementSP() +
-		dereferenceSP() +
-		"M=-M\n" +
-		"// done\n"
+func arithmeticNeg() []string {
+	var lines []string
+	lines = append(lines, comment("Negating x"))
+	lines = append(lines, decrementSP()...)
+	lines = append(lines, dereferenceSP()...)
+	lines = append(lines, "M=-M")
+	return indentedLines(lines...)
 }
 
-func arithmeticComparison(comparison string) string {
-	ret := fmt.Sprintf("// Comparing x and y using %s\n", comparison) +
-		loadXY() + // D contains y, M contains x, A contains SP
-		"D=M-D\n" + // D contains x-y
-		"M=-1\n" + // save "true" to top of stack
-		fmt.Sprintf("@DONE%d\n", doneCount) + // A contains address of code to jump to if condition satisfied; M no longer contains top of stack!
-		fmt.Sprintf("D;%s\n", comparison) + // jump ahead in code if condition satisfied
-		dereferenceSP() + // reset A & M
-		"M=0\n" + // save "false" to top of stack
-		fmt.Sprintf("(DONE%d)\n", doneCount) +
-		"// done\n"
+func arithmeticComparison(comparison string) []string {
+	var lines []string
+	lines = append(lines, comment(fmt.Sprintf("Comparing x and y using %s", comparison)))
+	lines = append(lines, loadXY()...)                         // D contains y, M contains x, A contains SP
+	lines = append(lines, "D=M-D")                             // D contains x-y
+	lines = append(lines, "M=-1")                              // save "true" to top of stack
+	lines = append(lines, fmt.Sprintf("@DONE%d\n", doneCount)) // A contains address of code to jump to if condition satisfied; M no longer contains top of stack!
+	lines = append(lines, fmt.Sprintf("D;%s\n", comparison))   // jump ahead in code if condition satisfied
+	lines = append(lines, dereferenceSP()...)                  // reset A & M
+	lines = append(lines, "M=0")                               // save "false" to top of stack
+	lines = append(lines, fmt.Sprintf("(DONE%d)", doneCount))
+	lines = append(lines, comment("done"))
 	doneCount++
-	return ret
+	return indentedLines(lines...)
 }
 
-func arithmeticEq() string {
+func arithmeticEq() []string {
 	return arithmeticComparison("JEQ")
 }
 
-func arithmeticGt() string {
+func arithmeticGt() []string {
 	return arithmeticComparison("JGT")
 }
 
-func arithmeticLt() string {
+func arithmeticLt() []string {
 	return arithmeticComparison("JLT")
 }
 
-func arithmeticAnd() string {
-	return "// And-ing x and y\n" +
-		loadXY() +
-		"M=D&M\n" +
-		"// done\n"
+func arithmeticAnd() []string {
+	var lines []string
+	lines = append(lines, comment("And-ing x and y"))
+	lines = append(lines, loadXY()...)
+	lines = append(lines, "M=D&M")
+	return indentedLines(lines...)
 }
 
-func arithmeticOr() string {
-	return "// Or-ing x and y\n" +
-		loadXY() +
-		"M=D|M\n" +
-		"// done\n"
+func arithmeticOr() []string {
+	var lines []string
+	lines = append(lines, comment("Or-ing x and y"))
+	lines = append(lines, loadXY()...)
+	lines = append(lines, "M=D|M")
+	return indentedLines(lines...)
 }
 
-func arithmeticNot() string {
-	return "// Not-ing x\n" +
-		decrementSP() +
-		dereferenceSP() +
-		"M=!M\n" +
-		"// done\n"
+func arithmeticNot() []string {
+	var lines []string
+	lines = append(lines, comment("Not-ing x"))
+	lines = append(lines, decrementSP()...)
+	lines = append(lines, dereferenceSP()...)
+	lines = append(lines, "M=!M")
+	return indentedLines(lines...)
 }
 
 // Return base address for segment
@@ -191,104 +197,117 @@ func standardizeSegment(segment string) string {
 }
 
 // Decrement Stack Pointer
-func decrementSP() string {
-	return "// Decrementing Stack Pointer\n" +
-		"@SP\n" +
-		"M=M-1\n" +
-		"// done\n"
+func decrementSP() []string {
+	return indentedLines(comment("Decrementing Stack Pointer"),
+		"@SP",
+		"M=M-1")
 }
 
 // Dereference Stack Pointer
-func dereferenceSP() string {
-	return "// Dereferencing Stack Pointer\n" +
-		"@SP\n" +
-		"A=M\n" +
-		"// done\n"
+func dereferenceSP() []string {
+	return indentedLines(comment("Dereferencing Stack Pointer"),
+		"@SP",
+		"A=M")
 }
 
 // Dereference Segment
-func dereferenceSegment(segment string, offset int) string {
-	comment := fmt.Sprintf("// Accessing value in %s %d\n", segment, offset)
-	var ret string
+func dereferenceSegment(segment string, offset int) []string {
 	switch segment {
 	case "pointer":
-		ret = dereferencePointer(offset)
+		return dereferencePointer(offset)
 	case "static":
-		ret = dereferenceStatic(offset)
+		return dereferenceStatic(offset)
 	case "temp":
-		ret = dereferenceTemp(offset)
+		return dereferenceTemp(offset)
 	default:
-		ret = dereferenceDefault(segment, offset)
+		return dereferenceDefault(segment, offset)
 	}
-	return comment + ret
 }
 
 // Dereference default
-func dereferenceDefault(segment string, offset int) string {
-	return fmt.Sprintf("@%s\n", standardizeSegment(segment)) + // goto segment e.g., LOCAL
-		"D=M\n" + // find address that LOCAL points to; store in data register
-		fmt.Sprintf("@%d\n", offset) + // load offset
-		"A=D+A\n" // add offset to base address, goto
+func dereferenceDefault(segment string, offset int) []string {
+	return indentedLines(comment(fmt.Sprintf("Accessing value in %s %d", segment, offset)),
+		fmt.Sprintf("@%s", standardizeSegment(segment)), // goto segment e.g., LOCAL
+		"D=M", // find address that LOCAL points to; store in data register
+		fmt.Sprintf("@%d", offset), // load offset
+		"A=D+A")                    // add offset to base address, goto
 }
 
 // Dereference pointer
-func dereferencePointer(offset int) string {
+func dereferencePointer(offset int) []string {
+	comment := comment(fmt.Sprintf("Accessing value in pointer %d", offset))
 	switch offset {
 	case 0:
-		return "@THIS\n"
+		return indentedLines(comment, "@THIS")
 	case 1:
-		return "@THAT\n"
+		return indentedLines(comment, "@THAT")
 	default:
 		panic(fmt.Sprintf("Error: invalid offset for pointer: %d", offset)) // TODO: this function should return an error
 	}
 }
 
 // Dereference static
-func dereferenceStatic(offset int) string {
+func dereferenceStatic(offset int) []string {
 	symbol := fmt.Sprintf("%s.%d", Filename, offset)
-	return fmt.Sprintf("@%s\n", symbol)
+	return indentedLines(comment(fmt.Sprintf("Accessing value in static %d", offset)),
+		fmt.Sprintf("@%s", symbol))
 }
 
 // Dereference temp
-func dereferenceTemp(offset int) string {
+func dereferenceTemp(offset int) []string {
 	addr := baseAddressForTemp + offset
 	if addr < 5 || addr > 15 {
 		panic(fmt.Sprintf("Error: invalid offset for temp: %d", offset))
 	}
-	return fmt.Sprintf("@%d\n", addr)
+	return indentedLines(comment(fmt.Sprintf("Accessing value in temp %d", offset)),
+		fmt.Sprintf("@%d", addr))
 }
 
 // Increment Stack Pointer
-func incrementSP() string {
-	return "// Incrementing Stack Pointer\n" +
-		"@SP\n" +
-		"M=M+1\n" +
-		"// done\n"
+func incrementSP() []string {
+	return indentedLines(comment("Incrementing Stack Pointer"),
+		"@SP",
+		"M=M+1")
 }
 
 // Load value from segment into data register
-func loadSegment(segment string, index int) string {
+func loadSegment(segment string, index int) []string {
+	var lines []string
 	switch segment {
 	case "constant":
-		return "// Loading constant\n" +
-			fmt.Sprintf("@%d\n", index) +
-			"D=A\n" +
-			"// done\n"
+		lines = append(lines, comment("Loading constant"))
+		lines = append(lines, fmt.Sprintf("@%d", index))
+		lines = append(lines, "D=A")
 	default:
-		return "// Loading segment\n" +
-			dereferenceSegment(segment, index) +
-			"D=M\n" +
-			"// done\n"
+		lines = append(lines, comment("Loading segment"))
+		lines = append(lines, dereferenceSegment(segment, index)...)
+		lines = append(lines, "D=M")
 	}
+	return indentedLines(lines...)
 }
 
 // Load top of stack (y) into D register, dereference second-to-top of stack (x)
-func loadXY() string {
-	return "// Accessing top two values from stack\n" +
-		decrementSP() +
-		dereferenceSP() +
-		"D=M\n" +
-		decrementSP() +
-		dereferenceSP() +
-		"// done\n"
+func loadXY() []string {
+	var lines []string
+	lines = append(lines, comment("Accessing top two values from stack"))
+	lines = append(lines, decrementSP()...)
+	lines = append(lines, dereferenceSP()...)
+	lines = append(lines, "D=M")
+	lines = append(lines, decrementSP()...)
+	lines = append(lines, dereferenceSP()...)
+	return indentedLines(lines...)
+}
+
+func comment(comment string) string {
+	return fmt.Sprintf("// %s", comment)
+}
+
+func indentedLines(lines ...string) []string {
+	var indentedLines []string
+	comment := lines[0]
+	indentedLines = append(indentedLines, comment)
+	for _, line := range lines[1:] {
+		indentedLines = append(indentedLines, "  "+line)
+	}
+	return indentedLines
 }
